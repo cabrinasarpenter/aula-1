5ad9c07846e3cd47edda9c28a153cb55980bfe19
cabrinasarpenter <243634335+cabrinasarpenter@users.noreply.github.com>
2025-11-12 16:16:27 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.title = "Sabrina Carpenter";
+
+magenta = 0
+
+function mudaCorDoTexto() {
+    magenta = magenta + 32
+document.body.style.color = `rgb(255,${magenta},255)`;
+}
+
+function zeraMagenta() {
+magenta = 0
+}
+
+botao = document.querySelector("button")
+
+botao.addEventListener("click", zeraMagenta)
+
+window.setInterval(mudaCorDoTexto, 100)
